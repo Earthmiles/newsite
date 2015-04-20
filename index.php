6e06c95a41acec9f9ad65345150114a4429bef06
Som Ghosh <som.ghosh@gmail.com>
2015-04-20 21:01:02 +0100

    changed the android app to new app link

--- index.php
+++ index.php
@@ -126,7 +126,7 @@
                         <a id="itunes-app-link" class="down-btn" href="http://app.appsflyer.com/id926440987?pid=website&c=mainpage"><img src="img/ios-download.png"></a>
                     </div>
                     <div class="down-btn12 col-xs-12 col-sm-4 col-md-4">
-                        <a id="playstore-app-link" class="down-btn" href="http://app.appsflyer.com/com.gmspartners.earthmiles?pid=website&c=mainpage">
+                        <a id="playstore-app-link" class="down-btn" href="http://app.appsflyer.com/com.gmspartnersltd.earthmiles?pid=website&c=mainpage">
                             <img alt="Get it on Google Play"
                                  src="img/google-play-en@2x.png" />
                         </a>
